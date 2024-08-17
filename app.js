942d2a2b4016c212f632e95e7f1d5a373a6e1ad7
Dev-BiiieL <dev-biiiel@outlook.com>
2024-08-17 16:17:06 -0300

    Subindo o desafio

---
+++ app.js
@@ -0,0 +1,12 @@
+const user = prompt("Qual o seu nome?")
+
+const age = Number(prompt("Qual a sua idade"))
+
+let number1 = Number(prompt("Digite um número: "))
+
+let number2 = Number(prompt("Digite outro número: "))
+
+let result = number1 + number2
+
+alert("Olá " + user + " ,você tem " + age + " anos, digitou "
+    + number1 + " e " + number2 + " e o total dessa soma é: " + result)
